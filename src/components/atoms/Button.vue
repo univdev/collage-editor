a0b9a35d827c7336fd0502cb01fcf09859132f19
박찬영 <pcydeveloper@gmail.com>
2022-04-28 23:43:51 +0900

    feat: Button, Select 컴포넌트 추가

---
+++ src/components/atoms/Button.vue
@@ -0,0 +1,43 @@
+<style lang="less" scoped>
+  .button {
+    .icon {
+      margin-right: 4px;
+    }
+  }
+</style>
+
+<template>
+  <van-button
+    v-bind="{ ...$attrs, ...$props }"
+    class="button"
+    :color="color"
+    @click="onClick"
+  >
+    <slot></slot>
+  </van-button>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+
+export default defineComponent({
+  name: 'AtomButton',
+  props: {
+    type: {
+      type: String as PropType<string>,
+      required: false,
+      default: null,
+    },
+    icon: {
+      type: String as PropType<string>,
+      required: false,
+      default: null,
+    },
+  },
+  methods: {
+    onClick(e: Event) {
+      this.$emit('click', e);
+    },
+  },
+});
+</script>
---
+++ src/components/atoms/Select.vue
@@ -0,0 +1,24 @@
+<style lang="less" scoped>
+
+</style>
+
+<template>
+  <select
+    v-bind="{ ...$attrs, ...$props }"
+  >
+    <slot></slot>
+  </select>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    value: {
+      required: false,
+      default: null,
+    },
+  },
+});
+</script>
