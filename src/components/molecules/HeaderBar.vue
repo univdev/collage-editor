b8fea8a18c5cbf53b146c4779b6247f44ae6bb83
박찬영 <pcydeveloper@gmail.com>
2022-04-30 20:39:39 +0900

    feat: 이미지 컴포넌트 추가

---
+++ src/components/molecules/HeaderBar.vue
@@ -0,0 +1,27 @@
+<style lang="less" scoped>
+  .header-bar {
+    width: 100%;
+    height: 60px;
+    padding: 0 16px;
+    display: flex;
+    align-items: center;
+    border-bottom: 1px solid #EEE;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+</style>
+
+<template>
+  <div class="header-bar">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'HeaderBar',
+});
+</script>
